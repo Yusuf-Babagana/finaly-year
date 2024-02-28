87ac69e39467776e740955d85bfa93efe2640f71
sushma1031 <sushmajram@gmail.com>
2024-02-28 17:30:42 +0530

    Re-structure search code

--- controllers/postSearchController.js
+++
@@ -1,5 +0,0 @@
-module.exports = (req, res) => {
-  const searchParams = new URLSearchParams(req.body);
-  const resultsUrl = `/departments/${req.params.code}/search?${searchParams.toString()}`
-  res.redirect(resultsUrl);
-};
