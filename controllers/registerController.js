39571dca4b4f77849ea4fb0f2de59a2ad44f2197
Tulasi2304 <tulasi.udupa23@gmail.com>
2024-02-29 14:32:50 +0530

    Add redirect to user exists

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -14,7 +14,6 @@ registerUserGet = async( req, res ) => {
 }
 
 registerUserPost = async( req, res ) => {
-    // console.log(req.body);
 
     const username = req.body.userName;
     const email = req.body.email;
@@ -23,19 +22,23 @@ registerUserPost = async( req, res ) => {
     console.log(username, email, hashedPassword, role);
 
     try {
-        const [userExists] = await pool.query('SELECT * FROM users WHERE username= ? ;',
-            [username]);
-        // console.log(userExists.length);
+        // const [userExists] = await pool.query('SELECT * FROM users WHERE username= ? ;',
+        //     [username]);
 
-        if( userExists.length !== 0 ){
-            res.send("User already exists!");
+        const [emailExists] = await pool.query('SELECT * FROM users WHERE email= ? ;',
+           [email]);
+
+
+        if( emailExists.length !== 0 ){
+            res.redirect('/register?error=alreadyexists');
         }
         else{
             const [role_id] = await pool.query('SELECT role_id FROM roles WHERE name= ? ;',[role]);
-            // console.log(role_id[0].role_id);
-            const [result] = await pool.query(`INSERT INTO users (username, email, role_id, password)
-                                            VALUES (?, ?, ?, ?)`, 
-                                            [username, email, role_id[0].role_id, hashedPassword]);
+            
+            await pool.query(`INSERT INTO users (username, email, role_id, password)
+                                VALUES (?, ?, ?, ?)`, 
+                                [username, email, role_id[0].role_id, hashedPassword]);
+                                
             console.log("New user created");
             res.send("Thank you for registering!");
         }
