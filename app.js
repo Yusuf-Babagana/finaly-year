d774ecbdaae650cfdb302e12e4fc2e489e18545a
sushma1031 <sushmajram@gmail.com>
2024-03-02 12:14:15 +0530

    Install and set up sessions functionality

--- app.js
+++ app.js
@@ -1,8 +1,50 @@
 const express = require("express");
+const session = require("express-session");
+const mysqlStore = require("express-mysql-session")(session);
 
-const getSemesterSubjectsContoller = require('./controllers/semesterController.js');
-const getSubjectMaterialsController = require('./controllers/subjectController.js');
-const getRegisterController = require('./controllers/registerController.js');
+const app = express();
+// app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const options = {
+  connectionLimit: 10,
+  host: "localhost",
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  createDatabaseTable: false,
+  schema: {
+		tableName: 'sessions',
+		columnNames: {
+			session_id: 'session_id',
+			expires: 'expires',
+			data: 'data'
+		}
+	},
+  endConnectionOnClose: true,
+};
+const sessionStore = new mysqlStore(options);
+
+const TWELVE_HOURS = 1000 * 60 * 60 * 12;
+
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    secret: process.env.SESS_SECRET,
+    cookie: {
+      maxAge: TWELVE_HOURS,
+    },
+  })
+);
+
+const PORT = 3000;
+
+const getSemesterSubjectsContoller = require("./controllers/semesterController.js");
+const getSubjectMaterialsController = require("./controllers/subjectController.js");
+const getRegisterController = require("./controllers/registerController.js");
 
 const registerGet = getRegisterController.registerUserGet;
 const registerPost = getRegisterController.registerUserPost;
@@ -12,32 +54,25 @@ const postSearchNotesController = require("./controllers/postSearchNotesControll
 const getSearchQPsController = require("./controllers/getSearchQPsController .js");
 const postSearchQPsController = require("./controllers/postSearchQPsController.js");
 
-const addNote = require('./controllers/addNoteController.js');
+const addNote = require("./controllers/addNoteController.js");
 const getAddController = addNote.getController;
 const postAddController = addNote.postController;
 
-const addQP = require('./controllers/addQPController.js');
+const addQP = require("./controllers/addQPController.js");
 const getAddQP = addQP.getController;
 const postAddQP = addQP.postController;
 
-const editNote = require('./controllers/editNoteController.js');
+const editNote = require("./controllers/editNoteController.js");
 const getEditNote = editNote.getController;
 const putEditNote = editNote.putController;
 
-const editQP = require('./controllers/editQPController.js');
+const editQP = require("./controllers/editQPController.js");
 const getEditQP = editQP.getController;
 const putEditQP = editQP.putController;
 
-const deleteDeptController = require('./controllers/deleteDeptController.js');
+const deleteDeptController = require("./controllers/deleteDeptController.js");
 const deleteNoteController = require("./controllers/deleteNoteController.js");
 const deleteQPController = require("./controllers/deleteQPController.js");
-
-const app = express();
-// app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send(`<h1>Home.<h1>`);
