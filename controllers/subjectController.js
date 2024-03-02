7581506688d7d82914a188402d2491800979c640
Tulasi2304 <tulasi.udupa23@gmail.com>
2024-03-02 20:16:15 +0530

    Update subject materials frontend

--- controllers/subjectController.js
+++ controllers/subjectController.js
@@ -13,9 +13,12 @@ getSubjectMaterials = async (req, res) => {
         const [questionPapers] = await pool.query('SELECT * FROM question_papers WHERE subject_id = ? ;',
             [subject_id]);
 
-        const [syllabus] = await pool.query('SELECT SY.* FROM syllabuses SY, subjects S WHERE S.code = ? AND S.syllabus_id = SY.id ;', [subjectCode]);
+            const [syllabus] = await pool.query('SELECT SY.* FROM syllabuses SY, subjects S WHERE S.code = ? AND S.syllabus_id = SY.id ;', [subjectCode]);
 
-        res.render('subjectMaterials', {subjectCode: subjectCode, notes: notes, questionPapers : questionPapers, syllabus: syllabus});
+        const [tags] = await pool.query('SELECT NT.note_id, T.tag_name FROM notes_tags NT, tags T, notes N WHERE N.subject_id = ? AND N.id = NT.note_id AND NT.tag_id = T.id;', [subject_id]);
+
+        res.render('subjectMaterials', {subjectCode: subjectCode, notes: notes, questionPapers : questionPapers, syllabus: syllabus, tags: tags});
+        
         // res.json({notes, questionPapers, syllabus});
     } catch (error) {
         console.log(error);
