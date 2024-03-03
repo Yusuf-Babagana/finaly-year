f16ea0dee7f9227c27948a330694c4a3c38cb76f
Tulasi2304 <tulasi.udupa23@gmail.com>
2024-03-03 15:47:21 +0530

    Add register

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -3,15 +3,24 @@ const bcrypt = require("bcrypt");
 
 registerUserGet = async( req, res ) => {
 
-    res.send("<h1>Register</h1>"+
-    "<form action='/register' method='post'>"+
-    "<input name='username', placeholder='UserName'>"+
-    "<input type='email', name='email', placeholder='Email id'>"+
-    "<input type='password', name='password', placeholder='Password'>"+
-    "<input type='checkbox', id='role', name='role', value='student'>"+
-    "<label for='role'> I am a student </label>"+
-        "<button type='submit' name='submit'>Register</button></form>");
-    //give radio button list for role
+    switch (req.query.error) {
+        case "alreadyexists":
+            message = "This email is already registered.";
+        break;
+        case "usernametaken":
+            message = "Username is already taken. Please select another username";
+        break;
+        default:
+            message = undefined;
+    }
+
+    const [username] = await pool.query('SELECT username FROM users');
+    const unames = [];
+    username.forEach((uname) => {
+        unames.push(uname.username);
+    })
+
+    res.render("register", { errorMessage: message, usernames: unames });
 }
 
 registerUserPost = async( req, res ) => {
@@ -19,17 +28,20 @@ registerUserPost = async( req, res ) => {
     const username = req.body.username;
     const email = req.body.email;
     const role = req.body.role;
-    // console.log(username, email, hashedPassword, role);
+    // console.log(username, email, role);
 
     try {
-        // const [userExists] = await pool.query('SELECT * FROM users WHERE username= ? ;',
-        //     [username]);
+        const [userExists] = await pool.query('SELECT * FROM users WHERE username= ? ;',
+            [username]);
         const [emailExists] = await pool.query('SELECT * FROM users WHERE email= ? ;',
            [email]);
 
-
         if( emailExists.length !== 0 ){
             res.redirect('/register?error=alreadyexists');
+        }
+        console.log(emailExists);
+        if( userExists.length !== 0){
+            res.redirect('/register?error=usernametaken');
         }
         else {
             const hashedPassword = await bcrypt.hash(req.body.password, 5);
@@ -38,11 +50,12 @@ registerUserPost = async( req, res ) => {
             const [result] = await pool.query(`INSERT INTO users (username, email, role_id, password)
                                 VALUES (?, ?, ?, ?)`, 
                                 [username, email, role_id[0].role_id, hashedPassword]);
+
                                 
             console.log("New user created");
             req.session.userId = result.insertId;
             req.session.role = role;
-            res.send("Thank you for registering!");
+            res.redirect('/');
         }   
     }
     catch (error) {
