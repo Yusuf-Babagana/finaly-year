b07af19de01f8eeb24e82bf9b95faa5bc239b709
Tulasi2304 <tulasi.udupa23@gmail.com>
2024-03-18 22:00:01 +0530

    Add internal question papers

--- controllers/subjectController.js
+++ controllers/subjectController.js
@@ -13,11 +13,13 @@ getSubjectMaterials = async (req, res) => {
         const [questionPapers] = await pool.query('SELECT * FROM question_papers WHERE subject_id = ? ;',
             [subject_id]);
 
-            const [syllabus] = await pool.query('SELECT SY.* FROM syllabuses SY, subjects S WHERE S.code = ? AND S.syllabus_id = SY.id ;', [subjectCode]);
+        const [syllabus] = await pool.query('SELECT SY.* FROM syllabuses SY, subjects S WHERE S.code = ? AND S.syllabus_id = SY.id ;', [subjectCode]);
 
         const [tags] = await pool.query('SELECT NT.note_id, T.tag_name FROM notes_tags NT, tags T, notes N WHERE N.subject_id = ? AND N.id = NT.note_id AND NT.tag_id = T.id;', [subject_id]);
 
-        res.render('subjectMaterials', {subjectCode: subjectCode, notes: notes, questionPapers : questionPapers, syllabus: syllabus, tags: tags});
+        const [test_qps] = await pool.query('SELECT * FROM test_papers WHERE subject_id = ? ;', [subject_id]);
+
+        res.render('subjectMaterials', {deptCode: req.params.code, subjectCode: subjectCode, notes: notes, questionPapers : questionPapers, syllabus: syllabus, tags: tags, test_qps : test_qps});
     } catch (error) {
         console.log(error);
     }
