45193d4f37dc0f54e51751af2258610bb595a2e5
Tulasi2304 <tulasi.udupa23@gmail.com>
2024-03-19 17:56:54 +0530

    Merge final-fixes to changes

--- controllers/subjectController.js
+++ controllers/subjectController.js
@@ -17,13 +17,9 @@ getSubjectMaterials = async (req, res) => {
 
         const [tags] = await pool.query('SELECT NT.note_id, T.tag_name FROM notes_tags NT, tags T, notes N WHERE N.subject_id = ? AND N.id = NT.note_id AND NT.tag_id = T.id;', [subject_id]);
 
-<<<<<<< HEAD
         const [test_qps] = await pool.query('SELECT * FROM test_papers WHERE subject_id = ? ;', [subject_id]);
 
-        res.render('subjectMaterials', {deptCode: req.params.code, subjectCode: subjectCode, notes: notes, questionPapers : questionPapers, syllabus: syllabus, tags: tags, test_qps : test_qps});
-=======
-        res.render('subjectMaterials', {subjectCode: subjectCode, notes: notes, questionPapers : questionPapers, syllabus: syllabus, tags: tags});
->>>>>>> 74d2195fe2d2686100ac291847030c97d98f5009
+        res.render('subjectMaterials', {subjectCode: subjectCode, notes: notes, questionPapers : questionPapers, syllabus: syllabus, tags: tags, test_qps : test_qps});
     } catch (error) {
         console.log(error);
     }
