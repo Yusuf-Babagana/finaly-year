a86f7dca87c5ab8de922233485d1af116b5c638d
sushma1031 <sushmajram@gmail.com>
2024-03-20 21:40:59 +0530

    Update subject deletion confirmation

--- public/confirmSubjectDelete.js
+++ public/confirmSubjectDelete.js
@@ -2,13 +2,11 @@ let sub = document.querySelectorAll(".deleteSub");
 let subId;
 sub.forEach((s) =>{
     s.addEventListener('click', (e)=>{
-        // subId = JSON.parse(JSON.stringify(e.target.nextElementSibling.dataset.subjectid));
-        // console.log(subId);
         if(window.confirm("Deleting this subject will remove it from all the departments currently offering it. Are you sure you wish to proceed?")){
-            console.log("hey inside");
+            console.log("Yes, delete subject");
         }
         else{
-            console.log("no");
+            console.log("Don't delete subject");
             e.preventDefault();
         }
     })
